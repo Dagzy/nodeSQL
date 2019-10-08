49783cf9d1d9175ac2a4138b24b28223f5dbad80
Dagzy <quincye.williams@gmail.com>
2019-10-08 01:36:58 -0400

    QW connecting to SQL DB, pushing to github

--- public/main.js
+++ public/main.js
@@ -1,8 +1,21 @@
 'use strict'
-document.onload = () => {
-    let partials = document.getElementsByClassName("partials");
+window.onload = () => {    
+    window.links = document.getElementsByClassName("nav-link");
+    window.partials = document.getElementsByClassName("partial")
+    for (const link of links) {
+        link.addEventListener('click', showArticle)
+    }
+}
+function showArticle(e){
+    e.preventDefault();
+    let t = e.target.innerText.toLowerCase().trim();
+    let partial = document.getElementById(t);
+    hideArticles();
+    partial.classList.remove("hidden");
+}
+function hideArticles(){
     for (const partial of partials) {
-        console.log(partial)
+        partial.classList.add("hidden")
     }
 }
 function register(e) {
--- server.js
+++ server.js
@@ -6,8 +6,11 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const port = 9001
 const root = path.join(__dirname, '/public')
+const mysqlConnect = require('./mysql')
 app.use(express.static(root))
-
+app.get('/users',(req, res)=>{
+    res.send("Words")
+})
 app.listen(port, ()=>{
     console.log(`Listening on port: ${port}`)
 })
